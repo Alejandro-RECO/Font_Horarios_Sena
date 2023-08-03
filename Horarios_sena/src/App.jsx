03c6f2f45dcc2c274467a4b750addbab52ac9d5e
Alejandro-RECO <130815506+Alejandro-RECO@users.noreply.github.com>
2023-08-03 12:09:51 -0500

    Inicio Proyecto
    Generamos archivo React,

---
+++ Horarios_sena/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { NavLeft } from './components/Nav_Principal_Izquierda/NPI.jsx'
+
+
+const App = () => {
+
+  return (
+    <>
+      <div className='Container'>
+        <NavLeft />
+      </div>
+    </>
+  )
+}
+
+export default App
